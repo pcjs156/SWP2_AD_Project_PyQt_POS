b7a6c16eaaf99ba1e6329a2e447cac53a62c32ea
yubin <itqkrdbqls1001@naver.com>
2020-11-21 18:50:27 +0900

    0. 우측 하단의 "POS 종료" 버튼을 누르면 POS 시작~끝까지의 판매 정보가 sales 디렉토리에 저장되도록 수정 1. gitignore 수정.. 했는데 sales 디렉토리의 파일이 자꾸 올라감 ㅠ 2. POS 구현/실행 파일을 분리함 -> run.py에서 실행할 것

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
-if __name__ == "__main__":
-    from CUI_POS.core import POSCore
+import sys
 
-    POSCore = POSCore()
-    POSCore.run()
+from PyQt5.QtWidgets import QApplication
+
+from CUI_POS.core import POSCore
+from GUI_POS.gui_pos import GUIPosWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    pos = GUIPosWindow()
+    pos.show()
+    app.exec_()
+
+    # pos_cui = POSCore()
+    # pos_cui.run()
